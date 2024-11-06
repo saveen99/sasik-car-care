7132fc961a6dae3c8acdb7a9d02771af9b7b5768
Saveen Maduranga <105966451+saveen99@users.noreply.github.com>
2024-11-06 20:26:07 +0530

    Update Home.jsx/ Update Index.html

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
               <div className="col-lg-7 px-5 text-start">
                   <div className="h-100 d-inline-flex align-items-center py-3 me-4">
                       <small className="fa fa-map-marker-alt text-primary me-2"></small>
-                      <small>123 Street, New York, USA</small>
+                      <small>123 Street, Main Road, Kurunegala</small>
                   </div>
                   <div className="h-100 d-inline-flex align-items-center py-3">
                       <small className="far fa-clock text-primary me-2"></small>
@@ -19,7 +19,7 @@ function Home() {
               <div className="col-lg-5 px-5 text-end">
                   <div className="h-100 d-inline-flex align-items-center py-3 me-4">
                       <small className="fa fa-phone-alt text-primary me-2"></small>
-                      <small>+012 345 6789</small>
+                      <small>+94 773 730 768</small>
                   </div>
                   <div className="h-100 d-inline-flex align-items-center">
                       <a className="btn btn-sm-square bg-white text-primary me-1" href=""><i className="fab fa-facebook-f"></i></a>
