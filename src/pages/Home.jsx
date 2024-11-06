86323f8585c19ba418d497eef190e878ac1898cb
Saveen Maduranga <105966451+saveen99@users.noreply.github.com>
2024-11-06 21:57:49 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -494,9 +494,9 @@ function Home() {
         <div className="row g-5">
         <div className="col-lg-3 col-md-6">
             <h4 className="text-light mb-4">Address</h4>
-            <p className="mb-2"><i className="fa fa-map-marker-alt me-3"></i>123 Street, New York, USA</p>
-            <p className="mb-2"><i className="fa fa-phone-alt me-3"></i>+012 345 67890</p>
-            <p className="mb-2"><i className="fa fa-envelope me-3"></i>info@example.com</p>
+            <p className="mb-2"><i className="fa fa-map-marker-alt me-3"></i>123 Street, Main Road, Kurunegala</p>
+            <p className="mb-2"><i className="fa fa-phone-alt me-3"></i>+94 773 730 768</p>
+            <p className="mb-2"><i className="fa fa-envelope me-3"></i>sasikcar@gmail.com</p>
             <div className="d-flex pt-2">
             <a className="btn btn-outline-light btn-social" href="#"><i className="fab fa-twitter"></i></a>
             <a className="btn btn-outline-light btn-social" href="#"><i className="fab fa-facebook-f"></i></a>
@@ -520,21 +520,20 @@ function Home() {
             <a className="btn btn-link" href="#">Vacuum Cleaning</a>
         </div>
         <div className="col-lg-3 col-md-6">
-            <h4 className="text-light mb-4">Newsletter</h4>
-            <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
-            <div className="position-relative mx-auto" style={{ maxWidth: '400px' }}>
-            <input className="form-control border-0 w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email" />
-            <button type="button" className="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
-            </div>
+            <img 
+                src="/src/assets/img/logo.ico" 
+                alt="logo" 
+            />
         </div>
+
         </div>
     </div>
     <div className="container">
         <div className="copyright">
         <div className="row">
             <div className="col-md-6 text-center text-md-start mb-3 mb-md-0">
-            &copy; <a className="border-bottom" href="#">Your Site Name</a>, All Right Reserved.
-            Designed By <a className="border-bottom" href="https://htmlcodex.com">HTML Codex</a>
+            &copy; <a className="border-bottom" href="#">Sasik Car Care</a>, All Right Reserved.
+            Designed By <a className="border-bottom" href="#">Team NIBM</a>
             </div>
             <div className="col-md-6 text-center text-md-end">
             <div className="footer-menu">
