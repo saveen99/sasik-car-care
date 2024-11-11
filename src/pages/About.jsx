d398cb2de270c3137a2463cf3e2021729c2e8110
Saveen Maduranga <105966451+saveen99@users.noreply.github.com>
2024-11-11 14:05:45 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -125,22 +125,22 @@ function About() {
               <div className="row g-4">
                   <div className="col-md-6 col-lg-3 text-center wow fadeIn" data-wow-delay="0.1s">
                       <i className="fa fa-check fa-2x text-white mb-3"></i>
-                      <h2 className="text-white mb-2" data-toggle="counter-up">1234</h2>
+                      <h2 className="text-white mb-2" data-toggle="counter-up">15</h2>
                       <p className="text-white mb-0">Years Experience</p>
                   </div>
                   <div className="col-md-6 col-lg-3 text-center wow fadeIn" data-wow-delay="0.3s">
                       <i className="fa fa-users-cog fa-2x text-white mb-3"></i>
-                      <h2 className="text-white mb-2" data-toggle="counter-up">1234</h2>
+                      <h2 className="text-white mb-2" data-toggle="counter-up">5</h2>
                       <p className="text-white mb-0">Expert Technicians</p>
                   </div>
                   <div className="col-md-6 col-lg-3 text-center wow fadeIn" data-wow-delay="0.5s">
                       <i className="fa fa-users fa-2x text-white mb-3"></i>
-                      <h2 className="text-white mb-2" data-toggle="counter-up">1234</h2>
+                      <h2 className="text-white mb-2" data-toggle="counter-up">500+</h2>
                       <p className="text-white mb-0">Satisfied Clients</p>
                   </div>
                   <div className="col-md-6 col-lg-3 text-center wow fadeIn" data-wow-delay="0.7s">
                       <i className="fa fa-car fa-2x text-white mb-3"></i>
-                      <h2 className="text-white mb-2" data-toggle="counter-up">1234</h2>
+                      <h2 className="text-white mb-2" data-toggle="counter-up">1000+</h2>
                       <p className="text-white mb-0">Complete Projects</p>
                   </div>
               </div>
