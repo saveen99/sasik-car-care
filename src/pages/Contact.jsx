d7a84e03991acf47904541286507df67e42caf83
Saveen Maduranga <105966451+saveen99@users.noreply.github.com>
2024-11-16 11:08:00 +0530

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -57,7 +57,7 @@ function Contact() {
                 </div>
               </div>
             </div>
-            <div className="col-md-6 wow fadeIn" data-wow-delay="0.1s">
+            <div className="col-md-12 wow fadeIn" data-wow-delay="0.1s">
               <iframe
                 className="position-relative rounded w-100 h-100"
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3954.456638460339!2d80.23295207373857!3d7.633941209266715!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae32d3e8417d03f%3A0x40a64d1c0d4c5656!2sSasik%20service%20station!5e0!3m2!1sen!2slk!4v1731300481086!5m2!1sen!2slk"
@@ -67,47 +67,7 @@ function Contact() {
                 tabIndex="0"
               ></iframe>
             </div>
-            <div className="col-md-6">
-              <div className="wow fadeInUp" data-wow-delay="0.2s">
-                <p className="mb-4">
-                  The contact form is currently inactive. Get a functional and working contact form with Ajax & PHP in a few minutes. Just copy and paste the files, add a little code and youre done.{' '}
-                  <a href="">Download Now</a>.
-                </p>
-                <form>
-                  <div className="row g-3">
-                    <div className="col-md-6">
-                      <div className="form-floating">
-                        <input type="text" className="form-control" id="name" placeholder="Your Name" />
-                        <label htmlFor="name">Your Name</label>
-                      </div>
-                    </div>
-                    <div className="col-md-6">
-                      <div className="form-floating">
-                        <input type="email" className="form-control" id="email" placeholder="Your Email" />
-                        <label htmlFor="email">Your Email</label>
-                      </div>
-                    </div>
-                    <div className="col-12">
-                      <div className="form-floating">
-                        <input type="text" className="form-control" id="subject" placeholder="Subject" />
-                        <label htmlFor="subject">Subject</label>
-                      </div>
-                    </div>
-                    <div className="col-12">
-                      <div className="form-floating">
-                        <textarea className="form-control" placeholder="Leave a message here" id="message" style={{ height: '100px' }}></textarea>
-                        <label htmlFor="message">Message</label>
-                      </div>
-                    </div>
-                    <div className="col-12">
-                      <button className="btn btn-primary w-100 py-3" type="submit">
-                        Send Message
-                      </button>
-                    </div>
-                  </div>
-                </form>
-              </div>
-            </div>
+            
           </div>
         </div>
       </div>
