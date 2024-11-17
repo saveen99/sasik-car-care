97559aa4e178f9518111a8c3a2cd450ed35c0ecc
Saveen Maduranga <105966451+saveen99@users.noreply.github.com>
2024-11-17 10:39:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ import Team from './pages/Team';
 import Testimonial from './pages/Testimonial';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Cookies from './pages/Cookies';
+import Help from './pages/Help';
+import FAQs from './pages/FAQs';
 
 
 function App() {
@@ -23,6 +26,9 @@ function App() {
           <Route path="/error" element={<Error />} />
           <Route path="/team" element={<Team />} />
           <Route path="/testimonial" element={<Testimonial />} />
+          <Route path="/cookies" element={<Cookies/>} />
+          <Route path="/help" element={<Help />} />
+          <Route path="/faqs" element={<FAQs />} />
         </Routes>
         <Footer />
       </BrowserRouter>
